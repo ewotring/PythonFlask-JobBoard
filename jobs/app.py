ae1583bd634622f3c5575b87ac1daa84eea515df
Erik Wotring <ewotring@gmail.com>
2020-04-25 21:12:03 -0500

    Complete module

--- jobs/app.py
+++ jobs/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import g
 from flask import request
+from flask import redirect
+from flask import url_for
 import sqlite3
 import datetime
 
@@ -52,4 +54,12 @@ def employer(employer_id):
 
 @app.route('/employer/<employer_id>/review')
 def review(employer_id, methods=('GET', 'POST')):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
